42d64ff162ebc5c6d9db834c09711ba26703f2a9
Dakota Polenz <polenzdakota@yahoo.com>
2015-11-20 19:40:04 -0800

    Added Needed scripts and basic methods to command
    Command implementation is very rudimentary. Not sure on how exactly we
    are going to implement the class.

---
+++ UnityRobotProj/Assets/Scripts/Command.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Contains a queue of actions picked by the player
+/// </summary>
+public class Command : MonoBehaviour {
+	private Queue<IActions> actions;
+
+	void Start () {
+		actions = new Queue<IActions> ();
+	}
+
+	/// <summary>
+	/// Adds the action to the action queue.
+	/// </summary>
+	/// <param name="addedAction">Added action.</param>
+	public void AddAction(IActions addedAction) {
+		actions.Enqueue (addedAction);
+	}
+
+	/// <summary>
+	/// Gets the next action. Returns null if the queue is empty
+	/// </summary>
+	/// <returns>The next action.</returns>
+	public IActions GetNextAction() {
+		if (actions.Count > 0) {
+			return actions.Dequeue ();
+		} else {
+			return null;
+		}
+	}
+}
