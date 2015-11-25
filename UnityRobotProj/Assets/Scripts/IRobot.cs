6decc680dd40f74da37bbc021b692ae30514ff93
Dakota Polenz <polenzdakota@yahoo.com>
2015-11-24 19:58:37 -0800

    Added methods in IRobot. Created first robot script for the primary robot
    All of the methods in Robot are unfinished.

--- UnityRobotProj/Assets/Scripts/IRobot.cs
+++ UnityRobotProj/Assets/Scripts/IRobot.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class IRobot : MonoBehaviour {
+public interface IRobot {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	/// <summary>
+	/// Moves the robot forward.
+	/// </summary>
+	/// <returns><c>true</c>, if forward was moved, <c>false</c> otherwise.</returns>
+	bool MoveForward();
+
+	/// <summary>
+	/// Rotates the robot right.
+	/// </summary>
+	void RotateRight();
+
+	/// <summary>
+	/// Rotates the robot left.
+	/// </summary>
+	void RotateLeft();
+
+	/// <summary>
+	/// Moves the robot to the given position.
+	/// </summary>
+	/// <returns><c>true</c>, if to position was moved, <c>false</c> otherwise.</returns>
+	/// <param name="pos">Position.</param>
+	bool MoveToPosition(Vector3 pos);
+
 }
---
+++ UnityRobotProj/Assets/Scripts/Robot.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Robot : MonoBehaviour, IRobot {
+
+	public Vector3 initialPosition = new Vector3(0,0,0);
+	public float moveDistance = 10;
+	private Vector3 currentPosition;
+
+	//Dx and Dy indicate the direction the robot is facing
+	//With dx = 1 and dy = 0 the robot is facing right and
+	//will move in that direction with a move forward command
+	public int dx = 1;
+	public int dy = 0;
+
+	// Use this for initialization
+	void Start () {
+		currentPosition.Set (initialPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// Moves the robot forward.
+	/// </summary>
+	/// <returns>true</returns>
+	/// <c>false</c>
+	public bool MoveForward() {
+		float nextX = (currentPosition.x + moveDistance) * dx;
+		float nextY = (currentPosition + moveDistance) * dy;
+		currentPosition.Set (nextX, nextY);
+
+		//TODO add check to see if the next position is valid.
+		return true;
+	}
+
+	/// <summary>
+	/// Rotates the robot right.
+	/// </summary>
+	public void RotateRight() {
+		int tmp = dy;
+		dy = -dx;
+		dx = tmp;
+		//TODO add rotation for the robot in the scene.
+	}
+
+	/// <summary>
+	/// Rotates the robot left.
+	/// </summary>
+	public void RotateLeft() {
+		int tmp = dx;
+		dx = -dy;
+		dy = tmp;
+		//TODO add rotation for the robot in the scene.
+	}
+
+	/// <summary>
+	/// Moves the robot to the given position.
+	/// </summary>
+	/// <returns>true</returns>
+	/// <c>false</c>
+	/// <param name="pos">Position.</param>
+	public bool MoveToPosition(Vector3 pos) {
+		currentPosition.Set (pos);
+
+		//TODO add check to see if position is valid.
+		return true;
+	}
+}
