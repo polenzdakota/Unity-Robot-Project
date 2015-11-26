41911cf966815301163870970c7c4378350fec0b
Dakota Polenz <polenzdakota@yahoo.com>
2015-11-26 14:37:18 -0800

    Added rotate right action

---
+++ UnityRobotProj/Assets/Scripts/Actions/MainRotateRight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainRotateRight : MonoBehaviour, IActions {
+	public GameObject mainRobot;
+
+	/// <summary>
+	/// Invokes the action of the command.
+	/// </summary>
+	/// <returns>true</returns>
+	/// <c>false</c>
+	public bool InvokeAction() {
+		mainRobot.GetComponent<Robot> ().RotateRight ();
+		return true;
+	}
+
+	/// <summary>
+	/// Undos the action.
+	/// </summary>
+	public void UndoAction() {
+		mainRobot.GetComponent<Robot> ().RotateLeft ();
+	}
+}
