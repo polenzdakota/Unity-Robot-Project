750cc2795049fa936d23e98b0137439e967471ce
Dakota Polenz <polenzdakota@yahoo.com>
2015-11-26 14:50:31 -0800

    Changed the Queue of actions to a list of actions
    This will make it easier to delete actions given an index. Iterating
    through the list will still be the same as if we had a queue.

--- UnityRobotProj/Assets/Scripts/Command.cs
+++ UnityRobotProj/Assets/Scripts/Command.cs
@@ -6,24 +6,24 @@ using System.Collections.Generic;
 /// Contains a queue of actions picked by the player
 /// </summary>
 public class Command : MonoBehaviour {
-	private Queue<IActions> actions;
+	private List<IActions> actions;
 
 	void Start () {
-		actions = new Queue<IActions> ();
+		actions = new List<IActions> ();
 	}
 
 	/// <summary>
-	/// Adds the action to the action queue.
+	/// Adds the action to the action list.
 	/// </summary>
 	/// <param name="addedAction">Added action.</param>
 	public void AddAction(IActions addedAction) {
-		actions.Enqueue (addedAction);
+		actions.Add (addedAction);
 	}
 
 	/// <summary>
-	/// Executes the actions in the queue.
+	/// Executes the actions in the action list.
 	/// </summary>
-	public void ExecuteQueue() {
+	public void ExecuteActions() {
 		foreach (IActions action in actions) {
 			bool valid = action.InvokeAction();
 			if (!valid) {
@@ -48,9 +48,18 @@ public class Command : MonoBehaviour {
 	/// <returns>The next action.</returns>
 	public IActions GetNextAction() {
 		if (actions.Count > 0) {
-			return actions.Dequeue ();
+			return actions[0];
 		} else {
 			return null;
 		}
+	}
+
+	/// <summary>
+	/// Removes the given action.
+	/// </summary>
+	/// <returns><c>true</c>, if action was removed, <c>false</c> otherwise.</returns>
+	/// <param name="index">Index.</param>
+	public bool RemoveAction(int index) {
+		return actions.Remove (index);
 	}
 }
