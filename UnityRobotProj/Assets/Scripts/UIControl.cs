35eb8ec2c9d47260239c605a943abfcbaef00bf3
Dakota Polenz <polenzdakota@yahoo.com>
2015-12-05 21:38:25 -0800

    Got UI element generation to work
    Still need to configure the prefabs to delete the component when clicked
    in the action panel. I will also need to tweak the layout of the action
    panel so that it doesn't look weird when you add a button. Side Note:
    Make sure you play the game at 16:9 since I really don't want to work on
    supporting multiple resolutions at this moment. Maybe we can work that
    in later if we have time.

--- UnityRobotProj/Assets/Scripts/UIControl.cs
+++ UnityRobotProj/Assets/Scripts/UIControl.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class UIControl : MonoBehaviour {
+	public GameObject actionPanel;
+	public GameObject TestButton;
 
 
 	public void ButtonPress(string strAction) {
+		print ("Trigger with action: " + strAction);
+		Vector3 dumb = new Vector3 (0, 0, 0);
+		Quaternion stuff = new Quaternion (0,0,0,0);
+		GameObject newButton = Instantiate (TestButton, dumb, stuff) as GameObject;
+		print (newButton.transform.position.x);
+		newButton.transform.SetParent (actionPanel.transform);
+
 		//TODO
 	}
 
