aeb81b2dadb6725c56db6d8069417dacbeb6c3cd
Dakota Polenz <polenzdakota@yahoo.com>
2015-12-05 21:58:48 -0800

    Buttons in the action panel now delete themselves on click

--- UnityRobotProj/Assets/Scripts/ActionButton.cs
+++ UnityRobotProj/Assets/Scripts/ActionButton.cs
@@ -3,13 +3,7 @@ using System.Collections;
 
 public class ActionButton : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void OnPress() {
+		Destroy (gameObject);
 	}
 }
