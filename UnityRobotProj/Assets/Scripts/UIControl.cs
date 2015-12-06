83e3cbd9250d09d94a32f5ef9b3a1040977efcec
Dakota Polenz <polenzdakota@yahoo.com>
2015-12-05 22:05:13 -0800

    Added documentation

--- UnityRobotProj/Assets/Scripts/UIControl.cs
+++ UnityRobotProj/Assets/Scripts/UIControl.cs
@@ -2,11 +2,17 @@
 using System.Collections;
 using UnityEngine.UI;
 
+/// <summary>
+/// Controls buttons and positions on the UI.
+/// </summary>
 public class UIControl : MonoBehaviour {
 	public GameObject actionPanel;
 	public GameObject TestButton;
 
-
+	/// <summary>
+	/// Action done when a button is pressed.
+	/// </summary>
+	/// <param name="strAction">String action.</param>
 	public void ButtonPress(string strAction) {
 		print ("Trigger with action: " + strAction);
 		Vector3 dumb = new Vector3 (0, 0, 0);
@@ -15,9 +21,14 @@ public class UIControl : MonoBehaviour {
 		print (newButton.transform.position.x);
 		newButton.transform.SetParent (actionPanel.transform);
 
-		//TODO
+		//TODO options for multiple buttons. We can use the string paran to 
+		//     Differentiate between the actions.
 	}
 
+	/// <summary>
+	/// Adds the action to panel.
+	/// </summary>
+	/// <param name="action">Action.</param>
 	public void AddActionToPanel(IActions action) {
 		//TODO
 	}
