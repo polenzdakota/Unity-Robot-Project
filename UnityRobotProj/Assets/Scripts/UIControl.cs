8be61a040384fb2cffff0db6892405a0297dc24e
Dakota Polenz <polenzdakota@yahoo.com>
2015-12-05 20:31:31 -0800

    Added stubs to UIControl

--- UnityRobotProj/Assets/Scripts/UIControl.cs
+++ UnityRobotProj/Assets/Scripts/UIControl.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class UIControl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+
+	public void ButtonPress(string strAction) {
+		//TODO
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void AddActionToPanel(IActions action) {
+		//TODO
 	}
 }
